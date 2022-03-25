8baf4c34cd722b2dabf4ef0d6b033ebb469a6625
alexbenavente-cloudreach <alex.benavente@cloudreach.com>
2022-03-25 14:25:09 -0400

    Updated data subnet route table

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,4 @@
+# CREATE MAIN VPC
 resource "aws_vpc" "main" {
   cidr_block = "192.168.0.0/16"
 
@@ -6,6 +7,7 @@ resource "aws_vpc" "main" {
   }
 }
 
+# CREATE SUBNETS
 resource "aws_subnet" "public" {
     vpc_id = aws_vpc.main.id
     cidr_block = "192.168.1.0/24"
@@ -39,6 +41,7 @@ resource "aws_subnet" "data" {
     }
 }
 
+#CREATE GATEWAYS
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -49,15 +52,32 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
+  connectivity_type = "public"
   subnet_id = aws_subnet.public.id
 
   tags = {
     Name = "talent-academy-nat-gateway"
   }
+  depends_on = [aws_internet_gateway.igw]
 }
 
+#CREATE ELASTIC IPs
 resource "aws_eip" "nat_eip" {
   vpc = true
+}
+
+#CREATE ROUTE TABLES
+resource "aws_route_table" "nat_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "nat-route-table"
+  }
 }
 
 resource "aws_route_table" "internet_route_table" {
@@ -73,20 +93,9 @@ resource "aws_route_table" "internet_route_table" {
   }
 }
 
-resource "aws_route_table" "nat_route_table" {
-  vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw.id
-  }
 
-  tags = {
-    Name = "nat-route-table"
-  }
-}
-
-# ASSOCIATE ROUTE TABLE -- APP LAYER
+# ASSOCIATE ROUTE TABLE -- PRIVATE LAYER
 resource "aws_route_table_association" "internet_route_table_association_private" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.nat_route_table.id
@@ -96,4 +105,10 @@ resource "aws_route_table_association" "internet_route_table_association_private
 resource "aws_route_table_association" "internet_route_table_association_public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.internet_route_table.id
+}
+
+# ASSOCIATE ROUTE TABLE -- DATA LAYER
+resource "aws_route_table_association" "internet_route_table_association_data" {
+  subnet_id      = aws_subnet.data.id
+  route_table_id = aws_route_table.nat_route_table.id
 }
